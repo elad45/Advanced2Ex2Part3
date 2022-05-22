9a418678126548b49fb9f8ba28c010e658e2a3df
Elad Vizenblit <eladelad7@gmail.com>
2022-05-22 15:14:54 +0300

    pull from master

--- reactEx1/src/ChatscreenComponents/AddFriend.js
+++ reactEx1/src/ChatscreenComponents/AddFriend.js
@@ -9,55 +9,46 @@ import './AddFriend.css'
 //using setFriends
 function AddFriend(props) {
     
-    // const [AllUsersNicknames,setUsersNicknames]  = useState("");
+    const [AllUsersNicknames,setUsersNicknames]  = useState("");
     
-    // const fetchAllNicknames = async() => {
-    //     const response = await fetch('http://localhost:5094/api/Users/GetAllUsers',{
-    //         method:'get',
-    //         headers: {
-    //             'Content-Type' : 'application/json'},
-    //     })
-    //     var allNickNames = await response.json();
-    //     setUsersNicknames(allNickNames)
-    // }
-    // useEffect(() =>{
-    //     fetchAllNicknames();
-    //     console.log(AllUsersNicknames);
-    // },[]);
+    const fetchAllNicknames = async() => {
+        const response = await fetch('http://localhost:5094/api/Users/GetAllUsers',{
+            method:'get',
+            headers: {
+                'Content-Type' : 'application/json'},
+        })
+        var allNickNames = await response.json();
+        setUsersNicknames(allNickNames)
+    }
+    useEffect(() =>{
+        fetchAllNicknames();
+        console.log(AllUsersNicknames);
+    },[]);
     
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const handleAdd = () => {
-        let friendID =document.getElementById("friendID").value
         let friendNick = document.getElementById("friendNick").value
-        let friendUser = props.AllUsernames.find(x => x == friendID)
-        
+        let friendUser = AllUsersNicknames.find(x => x == friendNick)
+        console.log(friendUser)
         if (friendUser) {
             //User can't add himself as a contact
-            if (props.loggingUsername==friendID) {
+            if (props.loggingUserNickname==friendNick) {
                 alert("User can't add himself as a contact");
                 return;
             }
             //User can't add a friend that already in his contact list
-            // if (props.userContactsMap.value(x => x == friendID)) {
-            //     alert("Friend already in contacts list");
-            //     return;
-            // }
-            
-            for (const [key, value] of props.userContactsMap) {
-                console.log(key)
-                if (key == friendID) {
-                    alert("Friend already in contacts list");
-                    return;  
-              }
+            if (props.userContacts.find(x=>x == friendNick)) {
+                alert("Friend already in contacts list");
+                return;
             }
             props.setFriends((currentFriends) => {
                 let newFriends = [...currentFriends];
-                props.userContactsMap.push(friendNick)
+                props.userContacts.push(friendNick)
                 newFriends.push(friendNick)
                 handleClose();
-                console.log(props.userContactsMap);
+                console.log(props.userContacts);
                 return newFriends;
             })
         }
@@ -75,8 +66,7 @@ function AddFriend(props) {
                     <Modal.Title>Add friend to chat</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <input placeholder = "Enter friend username" id = "friendID"></input>
-                    <input placeholder = "Enter friend nickname" id="friendNick"></input>
+                    <input placeholder="Enter friend's name" id="friendNick"></input>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
