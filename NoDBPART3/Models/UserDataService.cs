0aebf8311c660fb50db1d8483402c7e30141abe7
Elad Vizenblit <eladelad7@gmail.com>
2022-05-25 21:56:54 +0300

    bug fix

--- NoDBPART3/Models/UserDataService.cs
+++ NoDBPART3/Models/UserDataService.cs
@@ -31,12 +31,12 @@
                     var user = users.Find(x=> x.Id == userId);
                     var secondUserId = conversation.UsersList.Find(x => x != userId);
                     var secondUser = users.Find(x => x.Id == secondUserId);
-                     contact = new Contact(secondUser.Id, secondUser.Name, "http://localhost:7270");
+                     contact = new Contact(secondUser.Id, secondUser.Name, "localhost:5094");
                     if (!user.ContactsList.Any(contact => contact.Id == secondUser.Id))
                         user.AddContact(contact);
                     contact.Last = conversation.MessagesList.Last().Content;
                     contact.Lastdate = DateTime.Now;
-                    contact = new Contact(user.Id, user.Name, "http://localhost:7260");
+                    contact = new Contact(user.Id, user.Name, "localhost:5094");
                     if (!secondUser.ContactsList.Any(contact => contact.Id == user.Id))
                         secondUser.AddContact(contact);
                     // possibly not needed because of line 32
