462808429f2d2d6170a1a74e949641a7df01484f
Elad Vizenblit <eladelad7@gmail.com>
2022-05-25 16:39:48 +0300

    invite works

---
+++ NoDBPART3/Models/InvitationService.cs
@@ -0,0 +1,26 @@
+﻿namespace NoDBPART3.Models
+{
+    public class InvitationService
+    {
+
+        public bool Invite (string from,string to,string server)
+        {
+            UserDataService service = new UserDataService();
+            User userTo = service.GetAll().Find(user => user.Id == to);
+            if (userTo == null)
+                return false;
+
+            foreach (Contact c in userTo.ContactsList)
+            {
+                if (c.Id == from)
+                {
+                    return false;
+                }
+            }
+            Contact newContact = new Contact(from, from, server);
+            userTo.ContactsList.Add(newContact);
+            return true;
+
+        }
+    }
+}
